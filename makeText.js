552b82e90d2fda11f2bf422e546ff132f66ea62f
Brandon Boatright <bboatright013@gmail.com>
2020-08-29 11:45:45 -0400

    markov functions appropriately

--- makeText.js
+++ makeText.js
@@ -1,2 +1,25 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const axios = require('axios');
+const { MarkovMachine } = require('./markov');
 
+
+if(process.argv[2]== 'file'){
+    console.log(`.....generated text from file ${process.argv[3]}`);
+        fs.readFile(process.argv[3],'utf8', (err, data) => {
+            if (err) throw err;
+            let markov = new MarkovMachine(data);
+            console.log(markov.makeText());
+            })
+   }
+else if(process.argv[2]=='url') {
+    console.log(`.....generated text from website at ${process.argv[3]}`);
+    getWebData(process.argv[3]);
+}
+
+
+async function getWebData(url){
+    let page = await axios.get(url);
+    let markov = new MarkovMachine(page.data);
+    console.log(markov.makeText());
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,47 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const chains = {};
+    let tmpStorage = [];
+    let bank = [];
+    for(let i = 0; i < this.words.length; i++){
+      if(bank.includes(this.words[i]) == false){
+        bank.push(this.words[i]);
+        for(let y = 0; y < this.words.length; y++){
+          if(this.words[y] == this.words[i]){
+            tmpStorage.push(this.words[y + 1])
+          }
+        }
+        chains[this.words[i]] = tmpStorage;
+        tmpStorage = [];
+      }
+    }
+    this.chains = chains;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
-  }
+    let keys = Object.keys(this.chains);
+    let tmpArray = [];
+    tmpArray.push(keys[0]);
+    for(let i = 0; i < numWords; i++){
+      if(this.chains[tmpArray[i]] != undefined){
+        let random = getRandomNextWord(this.chains[tmpArray[i]].length);
+        tmpArray.push(this.chains[tmpArray[i]][random]);
+      } else {
+        let returnString = tmpArray.join(' ');
+        return returnString
+      }
+      }
+      let returnString = tmpArray.join(' ');
+      return returnString;
+    }
 }
+
+function getRandomNextWord(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+module.exports = { MarkovMachine };
